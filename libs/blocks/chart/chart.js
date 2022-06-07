b840defecf562762acefd18849cbed966871fdfe
Chris Millar <cmillar@adobe.com>
2022-06-07 06:45:54 -0600

    cleanup code. more dynamic.

--- libs/blocks/chart/chart.js
+++ libs/blocks/chart/chart.js
@@ -1,36 +1,42 @@
 import { loadScript, makeRelative } from '../../utils/utils.js';
 
-const options = {
-  tooltip: {},
-  legend: { data: ['Revenue'] },
-  yAxis: {},
-};
+const options = { tooltip: {}, yAxis: {} };
+
+function formatExcelDate(string) {
+  return +string > 99999
+    ? new Date(+string * 1000)
+    : new Date(Math.round((+string - (1 + 25567 + 1)) * 86400 * 1000));
+}
 
 export default async function init(el) {
   await loadScript('/libs/deps/echarts.min.js');
   const fqdnPath = el.querySelector(':scope > div > div').textContent;
   const path = makeRelative(fqdnPath);
   const resp = await fetch(path);
-  if (resp.ok) {
-    const json = await resp.json();
-    const entries = json.data.map((entry) => {
-      // Excel date
-      const excelDate = +entry.Day > 99999
-      ? new Date(+entry.Day * 1000)
-      : new Date(Math.round((+entry.Day - (1 + 25567 + 1)) * 86400 * 1000));
+  if (!resp.ok) return;
+  const json = await resp.json();
+  const xAxisData = json.data.map((entry) => {
+    const excelDate = formatExcelDate(entry.Day);
+    const month = excelDate.toLocaleString('default', { month: 'short' });
+    const day = excelDate.toLocaleString('default', { day: 'numeric' });
+    return `${day}-${month}`;
+  });
+  const seriesData = json.data.map((entry) => { return entry.Revenue });
 
-      const month = excelDate.toLocaleString('default', { month: 'short' });
-      const day = excelDate.toLocaleString('default', { day: 'numeric' });
-      return { key: `${day}-${month}`, value: entry.Revenue };
-    });
+  // 'Revenue'
+  const metric = Object.keys(json.data[0])[1];
+  // 'Billion'
+  const unit = json.data[0].Unit;
 
-    const xAxis = { data: entries.map((entry) => { return entry.key }) };
-    const series = [ { name: 'Revenue', type: 'bar', data: entries.map((entry) => { return entry.value }) } ];
+  options.legend = { data: [metric] };
+  options.xAxis = { data: xAxisData };
+  options.series = [{
+    name: metric,
+    type: 'bar',
+    data: seriesData,
+    tooltip: { valueFormatter: value => `${value} ${unit}` },
+  }];
 
-    options.xAxis = xAxis;
-    options.series = series;
-
-    const chart = echarts.init(el);
-    chart.setOption(options);
-  }
+  const chart = echarts.init(el);
+  chart.setOption(options);
 }
