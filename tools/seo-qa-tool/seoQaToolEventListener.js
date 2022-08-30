b81f9739412db2517f182d596b628e73e74c2230
rea01581 <rea01581@adobe.com>
2022-08-30 13:48:57 -0700

    Testing status code

--- tools/seo-qa-tool/seoQaToolEventListener.js
+++ tools/seo-qa-tool/seoQaToolEventListener.js
@@ -1,5 +1,5 @@
 document.addEventListener('seo-qa-tool', async (e) => {
-  const { default: sendToCaaS } = await import('./seo-qa-tool.js');
+  const { default: seaQaTool } = await import('./seo-qa-tool.js');
   const {url} = e.detail;
-  sendToCaaS({url});
+  seaQaTool({url});
 });
