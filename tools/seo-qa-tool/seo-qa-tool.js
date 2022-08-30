9c1dec94cc9f3f7c62b1039c9c61793f3f060538
rea01581 <rea01581@adobe.com>
2022-08-30 13:58:29 -0700

    Testing status code

--- tools/seo-qa-tool/seo-qa-tool.js
+++ tools/seo-qa-tool/seo-qa-tool.js
@@ -1,5 +1,18 @@
-const seaQaTool = ({url} = {}) => {
+
+const [setConfig, getConfig] = (() => {
+  let config = {};
+  return [
+    (c) => {
+      config = { ...config, ...c };
+      return config;
+    },
+    () => config,
+  ];
+})();
+
+const seaQaTool = ({url=''} = {}) => {
 console.log("hi")
+setConfig({url});
 console.log(url);
 };
 export default seaQaTool;
