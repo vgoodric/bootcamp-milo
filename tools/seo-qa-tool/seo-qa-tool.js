2c831ad5fced11076e4e4262622f6cb0d0ddab52
rea01581 <rea01581@adobe.com>
2022-08-31 12:45:39 -0700

    testing

--- tools/seo-qa-tool/seo-qa-tool.js
+++ tools/seo-qa-tool/seo-qa-tool.js
@@ -1,13 +1,26 @@
-async function retireveStatuscode(url){
-  await fetch(url).then(response => {
-    return response.status;
-  });
+function checkIfExists(name) {
+  if ($(name).length) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 
-async function seaQaTool (){
-  let status = retireveStatuscode(window.location.href)
-  console.log(status)
+function seaQaTool() {
+  fetch(window.location.href).then(response => {
+    if (response.status === 200) {
+      alert("Status Code is correct");
+    } else {
+      alert("Invalid status code");
+    }
+  });
+  var hi = checkIfExists("h1")
+  if (hi) {
+    console.log("yes")
+  } else {
+    console.log("no")
+  }
 }
 
 
