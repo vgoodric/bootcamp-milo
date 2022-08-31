2b375e9ac7c3761c89564d841301f64a9c21565b
rea01581 <rea01581@adobe.com>
2022-08-31 11:36:13 -0700

    testing

--- tools/seo-qa-tool/seo-qa-tool.js
+++ tools/seo-qa-tool/seo-qa-tool.js
@@ -1,16 +1,13 @@
-function UrlChecking(url){
-  
+function retireveStatuscode(url){
+  fetch(url).then(response => {
+    return response;
+  });
 }
 
 
 function seaQaTool (){
-  fetch(window.location.href).then(response => {
-    if (response.status === 200) {
-      console.log("Expected Status code")
-    } else if(response.status < 400 && response.status > 200) {
-      console.log("Hi")
-    }
-  });
+ let response = retireveStatuscode(window.location.href)
+ console.log(response.status)
 }
 
 
