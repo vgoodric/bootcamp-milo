368ea07136b6a08d2a047994218197c0e8aadccf
rea01581 <rea01581@adobe.com>
2022-08-31 11:44:27 -0700

    testing

--- tools/seo-qa-tool/seo-qa-tool.js
+++ tools/seo-qa-tool/seo-qa-tool.js
@@ -1,11 +1,11 @@
-function retireveStatuscode(url){
-  fetch(url).then(response => {
+async function retireveStatuscode(url){
+  await fetch(url).then(response => {
     return response.status;
   });
 }
 
 
-function seaQaTool (){
+async function seaQaTool (){
   let status = retireveStatuscode(window.location.href)
   console.log(status)
 }
