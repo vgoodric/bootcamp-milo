77308faa42d33b786b09ce2a4bb87f83cdfc3798
Ryan Clayton <rclayton@adobe.com>
2022-12-12 13:34:19 -0700

    WIP - adding lightbox feature.

--- libs/blocks/carousel/carousel.js
+++ libs/blocks/carousel/carousel.js
@@ -19,6 +19,15 @@ const base = miloLibs || codeRoot;
 
 const ARROW_NEXT_IMG = `<img class="next-icon" alt="Next icon" src="${base}/blocks/carousel/arrow.svg" height="10" width="16">`;
 const ARROW_PREVIOUS_IMG = `<img class="previous-icon" alt="Previous icon" src="${base}/blocks/carousel/arrow.svg" height="10" width="16">`;
+const LIGHTBOX_ICON = `<img class="expand-icon" alt="Expand carousel to full screen" src="${base}/blocks/carousel/expand.svg" height="14" width="20">`;
+const CLOSE_ICON = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+  <g transform="translate(-10500 3403)">
+    <circle cx="10" cy="10" r="10" transform="translate(10500 -3403)" fill="#707070"/>
+    <line y1="8" x2="8" transform="translate(10506 -3397)" fill="none" stroke="#fff" stroke-width="2"/>
+    <line x1="8" y1="8" transform="translate(10506 -3397)" fill="none" stroke="#fff" stroke-width="2"/>
+  </g>
+</svg>`;
+// const SWIPE_PROPS = { event: '', direction: '' };
 const KEY_CODES = {
   SPACE: 'Space',
   END: 'End',
@@ -29,92 +38,145 @@ const KEY_CODES = {
   ARROW_DOWN: 'ArrowDown',
 };
 
-// function getCarouselProps(props) {
-//   // Create an object of carousel properties to pass on
-//   return props;
-// }
-
 function decorateNextPreviousBtns() {
   const btnsArray = [];
   const previousBtn = createTag(
-    'button', 
-    { 
+    'button',
+    {
       class: 'carousel-button carousel-previous',
       'aria-label': 'Previous',
       'data-toggle': 'previous',
     },
-    ARROW_PREVIOUS_IMG
+    ARROW_PREVIOUS_IMG,
   );
 
   const nextBtn = createTag(
-    'button', 
-    { 
+    'button',
+    {
       class: 'carousel-button carousel-next',
       'aria-label': 'Next',
       'data-toggle': 'next',
     },
-    ARROW_NEXT_IMG
+    ARROW_NEXT_IMG,
   );
 
   btnsArray.push(previousBtn, nextBtn);
   return btnsArray;
 }
 
+function decorateLightboxButtons() {
+  const btnsArray = [];
+  const expandBtn = createTag(
+    'button',
+    {
+      class: 'lightbox-button carousel-expand',
+      'aria-label': 'Open in full screen',
+    },
+    LIGHTBOX_ICON,
+  );
+  const closeBtn = createTag(
+    'button',
+    {
+      class: 'lightbox-button carousel-close',
+      'aria-label': 'Close full screen carousel',
+    },
+    CLOSE_ICON,
+  );
+  btnsArray.push(expandBtn, closeBtn);
+  return btnsArray;
+}
+
 function decorateSlideIndicators(slides) {
   const indicatorDots = [];
 
-  slides.forEach((li, idx) => {
-    li = createTag('li', {
+  for (let i = 0; i < slides.length; i += 1) {
+    const li = createTag('li', {
       class: 'carousel-indicator',
       role: 'tab',
       tabindex: -1,
-      'data-index': idx,
+      'data-index': i,
       'aria-selected': false,
-      'aria-labelledby': `Viewing Slide ${idx + 1}`,
+      'aria-labelledby': `Viewing Slide ${i + 1}`,
     });
 
     // Inital active state
-    if (idx === 0) {
+    if (i === 0) {
       li.classList.add('active');
       li.setAttribute('tabindex', 0);
     }
-    indicatorDots.push(li)
-  });
+    indicatorDots.push(li);
+  }
   return indicatorDots;
 }
 
 function handleNext(nextElement, elements) {
   if (nextElement.nextElementSibling) {
     return nextElement.nextElementSibling;
-  } else {
-    // The last nextElementSibiling returns false
-    nextElement = elements[0];
-    return nextElement;
   }
-};
+  // The last nextElementSibiling returns false
+  // nextElement = elements[0];
+  const updateElement = elements[0];
+  return updateElement;
+}
 
 function handlePrevious(previousElment, elements) {
   if (previousElment.previousElementSibling) {
     return previousElment.previousElementSibling;
-  } else {
-    previousElment = elements[elements.length - 1];
-    return previousElment;
   }
-};
+  // previousElment = elements[elements.length - 1];
+  const updateElement = elements[elements.length - 1];
+  return updateElement;
+}
 
-function moveSlides(event, slideContainer, slides, nextPreviousBtns, slideIndicators, controlsContainer) {
+function handleLightboxButtons(lightboxBtns, el, slideWrapper) {
+  const curtain = createTag('div', { class: 'carousel-curtain is-open' });
+  // Handle Next/Previous Buttons
+  Array.from(lightboxBtns).forEach((button) => {
+    button.addEventListener('click', (event) => {
+      event.preventDefault();
+      if (button.classList.contains('carousel-expand')) {
+        el.classList.add('lightbox-active');
+        slideWrapper.append(curtain);
+      }
+      // close arousel lightbox
+      if (button.classList.contains('carousel-close')) {
+        el.classList.remove('lightbox-active');
+        curtain.remove();
+      }
+    }, true);
+  });
+
+  // Handle click outside of Carousel/Close btn to close lightbox
+  curtain.addEventListener('click', (event) => {
+    event.preventDefault();
+    el.classList.remove('lightbox-active');
+    curtain.remove();
+  }, true);
+}
+
+function moveSlides(event, carouselElements) {
+  // console.log('jump', jumpTo);
+  const {
+    slideContainer,
+    slides,
+    nextPreviousBtns,
+    slideIndicators,
+    controlsContainer,
+    direction,
+  } = carouselElements;
+  console.log('move slides direction', direction);
   // console.log('currentTarget, eventCode', event.currentTarget, event.code);
   let referenceSlide = slideContainer.querySelector('.reference-slide');
   let slideIndex = controlsContainer.querySelector('.active');
-  
+
   // Check if reference slide has been set
   if (!referenceSlide) {
     slides[slides.length - 1].classList.add('reference-slide');
     slides[slides.length - 1].style.order = '-1';
-    // track reference slide
+    // track reference slide - last slide initially
     referenceSlide = slides[slides.length - 1];
   }
-  
+
   // Remove class after being tracked
   referenceSlide.classList.remove('reference-slide');
   referenceSlide.style.order = null;
@@ -122,21 +184,23 @@ function moveSlides(event, slideContainer, slides, nextPreviousBtns, slideIndica
   slideIndex.setAttribute('tabindex', -1);
 
   // Update reference slide
-  if ((event.currentTarget).dataset.toggle === 'next' 
+  if ((event.currentTarget).dataset.toggle === 'next'
     || event.key === KEY_CODES.ARROW_RIGHT
-    || event.key === KEY_CODES.ARROW_DOWN) {
-      nextPreviousBtns[1].focus();
-      referenceSlide = handleNext(referenceSlide, slides);
-      slideIndex = handleNext(slideIndex, slideIndicators);
-      slideContainer?.classList.remove('is-reversing');
-  } 
-  if ((event.currentTarget).dataset.toggle === 'previous' 
+    || event.key === KEY_CODES.ARROW_DOWN
+    || (direction === 'left' && event.type === 'touchend')) {
+    nextPreviousBtns[1].focus();
+    referenceSlide = handleNext(referenceSlide, slides);
+    slideIndex = handleNext(slideIndex, slideIndicators);
+    slideContainer?.classList.remove('is-reversing');
+  }
+  if ((event.currentTarget).dataset.toggle === 'previous'
     || event.key === KEY_CODES.ARROW_LEFT
-    || event.key === KEY_CODES.ARROW_UP) {
-      nextPreviousBtns[0].focus();
-      referenceSlide = handlePrevious(referenceSlide, slides);
-      slideIndex = handlePrevious(slideIndex, slideIndicators);
-      slideContainer.classList.add('is-reversing');
+    || event.key === KEY_CODES.ARROW_UP
+    || (direction === 'right' && event.type === 'touchend')) {
+    nextPreviousBtns[0].focus();
+    referenceSlide = handlePrevious(referenceSlide, slides);
+    slideIndex = handlePrevious(slideIndex, slideIndicators);
+    slideContainer.classList.add('is-reversing');
   }
   referenceSlide.classList.add('reference-slide');
   referenceSlide.style.order = '-1';
@@ -146,14 +210,16 @@ function moveSlides(event, slideContainer, slides, nextPreviousBtns, slideIndica
   slideIndex.setAttribute('tabindex', 0);
 
   // Loop over all siblings and update their order
-  let i, j, ref;
-  for(
-      i = j = 2, ref = slides.length;
-      2 <= ref ? j <= ref : j >= ref;
-      i = 2 <= ref ? ++j : --j
+  let i;
+  let j;
+  let ref;
+  for (
+    i = j = 2, ref = slides.length;
+    ref >= 2 ? j <= ref : j >= ref;
+    i = ref >= 2 ? ++j : --j
   ) {
-      referenceSlide = handleNext(referenceSlide, slides);
-      referenceSlide.style.order = i;
+    referenceSlide = handleNext(referenceSlide, slides);
+    referenceSlide.style.order = i;
   }
 
   /*
@@ -161,37 +227,89 @@ function moveSlides(event, slideContainer, slides, nextPreviousBtns, slideIndica
    * Delay time matches animation time
   */
   slideContainer.classList.remove('is-ready');
-  return setTimeout(function () {
-    return slideContainer.classList.add('is-ready');
-  }, 60);
+  return setTimeout(() => slideContainer.classList.add('is-ready'), 60);
 }
 
-function handleNextPreviousEvents(element, nextPreviousBtns, slideContainer, slides, slideIndicators, controlsContainer) {
+const getSwipeDistance = (start, end) => {
+  if (end === 0) {
+    const updateStart = 0;
+    return Math.abs(updateStart - end);
+  }
+  return Math.abs(start - end);
+};
+
+const getSwipeDirection = (swipe, swipeDistance) => {
+  const { xDistance } = swipeDistance;
+
+  if (xDistance !== swipe.xStart && xDistance > swipe.xMin) {
+    return (swipe.xEnd > swipe.xStart) ? 'right' : 'left';
+  }
+
+  return undefined;
+};
+
+/**
+  * Mobile swipe/touch direction detection
+  */
+function mobileSwipeDetect(carouselElements) {
+  const { el } = carouselElements;
+  const swipe = { xMin: 50 };
+
+  el.addEventListener('touchstart', (event) => {
+    const touch = event.touches[0];
+    swipe.xStart = touch.screenX;
+  });
+
+  el.addEventListener('touchmove', (event) => {
+    const touch = event.touches[0];
+    swipe.xEnd = touch.screenX;
+  });
+
+  el.addEventListener('touchend', (event) => {
+    const swipeDistance = {};
+    swipeDistance.xDistance = getSwipeDistance(swipe.xStart, swipe.xEnd);
+    carouselElements.direction = getSwipeDirection(swipe, swipeDistance);
+
+    // reset end swipe values
+    swipe.xStart = 0;
+    swipe.xEnd = 0;
+
+    if (swipeDistance.xDistance > swipe.xMin) {
+      moveSlides(event, carouselElements);
+    }
+  });
+}
+
+function handleNextPreviousEvents(carouselElements) {
+  const { el, nextPreviousBtns, slideIndicators } = carouselElements;
+
   // Handle Next/Previous Buttons
-  Array.from(nextPreviousBtns).forEach(function(btn) {
-    btn.addEventListener('click',  function (event) {
-      moveSlides(event, slideContainer, slides, nextPreviousBtns,slideIndicators, controlsContainer);
+  Array.from(nextPreviousBtns).forEach((btn) => {
+    btn.addEventListener('click', (event) => {
+      moveSlides(event, carouselElements);
     });
   });
 
-  // Handle keyboard navigation
-  element.addEventListener('keydown', (event) => {
-    // onKeyDown(event, slideContainer, slides);
+  // // Handle keyboard navigation
+  el.addEventListener('keydown', (event) => {
     // console.log(event.key);
-    if(event.key === KEY_CODES.ARROW_RIGHT
+    if (event.key === KEY_CODES.ARROW_RIGHT
       || event.key === KEY_CODES.ARROW_LEFT
       || event.key === KEY_CODES.ARROW_UP
-      || event.key === KEY_CODES.ARROW_DOWN)
-    moveSlides(event, slideContainer, slides, nextPreviousBtns, slideIndicators, controlsContainer);
+      || event.key === KEY_CODES.ARROW_DOWN) { moveSlides(event, carouselElements); }
   });
 
-  // Handle slide indictors
-  Array.from(slideIndicators).forEach(function(li) {
-    li.addEventListener('click',  function (event) {
-      console.log('slide indicator', li)
-      // moveSlides(event, slideContainer, slides, nextPreviousBtns, slideIndicators);
+  // // Handle slide indictors
+  Array.from(slideIndicators).forEach((li) => {
+    li.addEventListener('click', () => {
+      console.log('slide indicator', li.dataset.index);
+      // const jumpTo = li.dataset.index;
+      // moveSlides(event, carouselElements, jumpTo);
     });
   });
+
+  // Swipe Events
+  mobileSwipeDetect(carouselElements);
 }
 
 export default function init(el) {
@@ -200,11 +318,7 @@ export default function init(el) {
 
   const keyDivs = el.querySelectorAll(':scope > div > div:first-child');
   const carouselName = keyDivs[0].textContent;
-  // const timer = getCarouselProps(keyDivs);
-  // console.log('timer', timer);
-
   const parentArea = el.closest('.fragment') || document;
-
   const candidateKeys = parentArea.querySelectorAll('div.section-metadata > div > div:first-child');
   const slides = [...candidateKeys].reduce((rdx, key) => {
     if (key.textContent === 'carousel' && key.nextElementSibling.textContent === carouselName) {
@@ -224,11 +338,26 @@ export default function init(el) {
   const controlsContainer = createTag('div', { class: 'carousel-controls' });
 
   fragment.append(...slides);
+  const slideWrapper = createTag('div', { class: 'carousel-wrapper' });
   const slideContainer = createTag('div', { class: 'carousel-slides' }, fragment);
 
-  el.replaceChildren(slideContainer);
+  // el.replaceChildren(slideContainer);
+  if (el.classList.contains('lightbox')) {
+    const lightboxBtns = decorateLightboxButtons();
+    slideWrapper.append(slideContainer, ...lightboxBtns);
 
-  const dotsUl = createTag('ul',{ 
+    handleLightboxButtons(lightboxBtns, el, slideWrapper);
+  } else {
+    slideWrapper.append(slideContainer);
+  }
+  /*
+  * Remove block property DOM elements before adding updated slide DOM
+  * replaceChildren() api not supported by Safari 13.1
+  */
+  el.textContent = '';
+  el.append(slideWrapper);
+
+  const dotsUl = createTag('ul', {
     class: 'carousel-indicators',
     role: 'tablist',
     tabindex: 0,
@@ -238,12 +367,22 @@ export default function init(el) {
 
   el.append(...nextPreviousBtns, controlsContainer);
 
-  parentArea.addEventListener('milo:deferred', (e) => {
+  parentArea.addEventListener('milo:deferred', () => {
     const images = el.querySelectorAll('img[loading="lazy"]');
     images.forEach((img) => {
       img.removeAttribute('loading');
     });
   });
 
-  handleNextPreviousEvents(el, nextPreviousBtns, slideContainer, slides, slideIndicators, controlsContainer);
+  const carouselElements = {
+    el,
+    nextPreviousBtns,
+    slideContainer,
+    slides,
+    slideIndicators,
+    controlsContainer,
+    direction: undefined,
+  };
+
+  handleNextPreviousEvents(carouselElements);
 }
