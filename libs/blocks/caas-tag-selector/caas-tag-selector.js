7320aa38d354048b69de2c6d51875c453a68efeb
Jason Slavin <slavin@adobe.com>
2023-04-03 10:00:19 -0700

    Fetching tags

--- libs/blocks/caas-tag-selector/caas-tag-selector.js
+++ libs/blocks/caas-tag-selector/caas-tag-selector.js
@@ -1,8 +1,26 @@
-import { html, render } from '../../deps/htm-preact.js';
+import { html, render, useState, useEffect } from '../../deps/htm-preact.js';
 
 function CaasTagSelector() {
+  const [tags, setTags] = useState([]);
+
+  useEffect(() => {
+    async function fetchData () {
+      const tagUrl = 'https://www.adobe.com/chimera-api/tags';
+      const tagResp = await fetch(tagUrl);
+      if (!tagResp.ok) return {};
+      const json = await tagResp.json();
+      return json;
+    };
+    const data = fetchData();
+    console.log('data', data);
+    setTags(data);
+  }, []);
+
   return html`
-    <h1 class="cts-heading">Caas Tag Selector</h1>
+    <div>
+      <h1 class="cts-heading">Caas Tag Selector</h1>
+      <p>${tags[0]?.namespaces?.caas?.name}<p/>
+    </div>
   `;
 }
 
