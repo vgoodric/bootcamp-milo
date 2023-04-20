2f563e2eb34f48bba09a8c14d43501055948e06f
Chris Millar <cmillar@adobe.com>
2023-04-19 19:12:47 -0600

    More smoke

--- libs/blocks/amt/amt.js
+++ libs/blocks/amt/amt.js
@@ -1,4 +1,7 @@
-import { getConfig, loadScript } from '../../utils/utils.js';
+import { getConfig, loadScript, createTag } from '../../utils/utils.js';
+
+const ORIG_PATH = '/drafts/cmillar/adobe-digital-experiences-case-study-de';
+const MOD_PATH = '/drafts/cmillar/adobe-digital-experiences-case-study';
 
 async function getMd(path) {
   const resp = await fetch(path);
@@ -6,7 +9,26 @@ async function getMd(path) {
   return resp.text();
 }
 
+async function handlePreview() {
+  const opts = { method: 'POST' };
+  const resp = await fetch(`https://admin.hlx.page/preview/adobecom/milo/main${ORIG_PATH}`, opts);
+  if (!resp.ok) console.log('something went wrong');
+  window.open(ORIG_PATH, '_blank');
+}
+
+function makeButton(area) {
+  const button = createTag('button', { class: 'con-button blue' }, 'Preview');
+  button.addEventListener('click', handlePreview);
+  area.append(button);
+}
+
 export default async function init(el) {
+  const titleArea = el.querySelector(':scope > div:first-of-type');
+  const div = el.querySelector(':scope > div:last-of-type');
+  div.classList.add('amt-monaco-wrapper');
+  titleArea.classList.add('amt-title');
+  makeButton(titleArea);
+
   const config = getConfig();
   const base = config.miloLibs || config.codeRoot;
 
@@ -15,10 +37,10 @@ export default async function init(el) {
   require.config({ paths: { vs: `${base}/deps/amt/vs` } });
 
   window.require(['vs/editor/editor.main'], async () => {
-    const diffEditor = window.monaco.editor.createDiffEditor(el, { renderSideBySide: false });
+    const diffEditor = window.monaco.editor.createDiffEditor(div, { renderSideBySide: false });
 
-    const originalMd = getMd('/drafts/cmillar/adobe-digital-experiences-case-study-de.md');
-    const modifiedMd = getMd('/drafts/cmillar/adobe-digital-experiences-case-study.md');
+    const originalMd = getMd(`${ORIG_PATH}.md`);
+    const modifiedMd = getMd(`${MOD_PATH}.md`);
 
     Promise.all([originalMd, modifiedMd]).then((result) => {
       const original = window.monaco.editor.createModel(result[0], 'markdown');
