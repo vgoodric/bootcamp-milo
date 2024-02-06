762f330eb62eba90ca371287dd40ba58eef4cd80
mark <markp@adobe.com>
2024-02-06 13:31:54 -0800

    Add got new variant

--- libs/blocks/marquee/marquee.js
+++ libs/blocks/marquee/marquee.js
@@ -79,6 +79,7 @@ export async function loadMnemonicList(foreground) {
 }
 
 export default async function init(el) {
+  if (el.classList.contains('new-variant')) console.log('got it');
   const excDark = ['light', 'quiet'];
   if (!excDark.some((s) => el.classList.contains(s))) el.classList.add('dark');
   const children = el.querySelectorAll(':scope > div');
