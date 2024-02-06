87153dd99da353a952432a1aafbc935889336224
Benjamin Torres <bentorres@gmail.com>
2024-02-06 13:37:08 -0800

    add new element

--- libs/blocks/marquee/marquee.js
+++ libs/blocks/marquee/marquee.js
@@ -79,6 +79,11 @@ export async function loadMnemonicList(foreground) {
 }
 
 export default async function init(el) {
+  if(el.classList.contains("new-variant")){
+    console.log('has new element');
+  }else{
+    console.log('not here')
+  }
   const excDark = ['light', 'quiet'];
   if (!excDark.some((s) => el.classList.contains(s))) el.classList.add('dark');
   const children = el.querySelectorAll(':scope > div');
