a0d60bde7b556d0c4d3a6c58e09243772e1813d0
mark <markp@adobe.com>
2024-02-06 13:46:27 -0800

    Add got it in new branch

--- libs/blocks/marquee/marquee.js
+++ libs/blocks/marquee/marquee.js
@@ -79,7 +79,7 @@ export async function loadMnemonicList(foreground) {
 }
 
 export default async function init(el) {
-  if (el.classList.contains('new-variant')) console.log('got it');
+  if (el.classList.contains('new-variant')) console.log('got it...in the new branch!');
   const excDark = ['light', 'quiet'];
   if (!excDark.some((s) => el.classList.contains(s))) el.classList.add('dark');
   const children = el.querySelectorAll(':scope > div');
