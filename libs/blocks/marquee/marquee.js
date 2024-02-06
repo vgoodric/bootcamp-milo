6203024598b7b91f34bf8decf723822a5c91ed0a
Alexander Ivanov <aivanov@adobe.com>
2024-02-06 14:05:29 -0800

    Alex ai-main-3

--- libs/blocks/marquee/marquee.js
+++ libs/blocks/marquee/marquee.js
@@ -81,7 +81,7 @@ export async function loadMnemonicList(foreground) {
 export default async function init(el) {
 
   if (el.classList.contains('new-variant')) {
-    console.log('aivanov-main-2');
+    console.log('aivanov-main-3');
   }
 
   const excDark = ['light', 'quiet'];
