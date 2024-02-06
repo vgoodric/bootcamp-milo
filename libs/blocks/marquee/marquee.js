3110619ce51ea073b5028fd69652dbea5ef3237b
Christine Lin <chrlin@adobe.com>
2024-02-06 13:46:27 -0800

    chrlin - commit 2

--- libs/blocks/marquee/marquee.js
+++ libs/blocks/marquee/marquee.js
@@ -81,7 +81,7 @@ export async function loadMnemonicList(foreground) {
 export default async function init(el) {
 
   if (el.classList.contains('new-variant')) {
-    console.log('has new-variant');
+    console.log('has new-variant DIFFERENT');
   }
 
   const excDark = ['light', 'quiet'];
